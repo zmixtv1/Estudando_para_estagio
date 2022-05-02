db50d332e1bd617377887c42a80cd2975c6f7a11
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2022-05-02 16:50:43 -0300

    Update cadastrar.php

--- site/cadastrar.php
+++ site/cadastrar.php
@@ -10,20 +10,10 @@ $sql = "select count(*) as total from usuario where usuario = '$usuario'";
 $result = mysqli_query($conexao, $sql);
 $row = mysqli_fetch_assoc($result);
 
-if($row['total'] == 1) {
-	$_SESSION['usuario_existe'] = true;
-	header('Location: cadastro.php');
-	exit;
-}
-
-$sql = "INSERT INTO usuario (nome, usuario, senha, data_cadastro) VALUES ('$nome', '$usuario', '$senha', NOW())";
+$sql = "INSERT INTO usuario (nome, usuario, senha) VALUES ('$nome', '$usuario', '$senha')";
 
 if($conexao->query($sql) === TRUE) {
 	$_SESSION['status_cadastro'] = true;
 }
 
-$conexao->close();
-
-header('Location: cadastro.php');
-exit;
 ?>

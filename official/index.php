4fabd4ce3d042a0d0624a53b9b288e5d84de8f76
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2022-05-06 13:37:15 -0300

    ,

--- official/index.php
+++ official/index.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,29 +10,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;900&display=swap" rel="stylesheet">
-    <title>Cadastro</title>
+    <style></style>
+<title>Cadastro</title>
 </head>
 <body>
     <div class="container">
-        <div class="header">
-            <h1>Cadastro</h1>
+        <div>
+            <form action="cadastro.php" method="post">
+                <div class="cadastro">
+                    <h1>Cadastro</h1>
+                    <p class="aviso" name="aviso">Usuário já cadastrado!</p>
+                </div>
+                <div class="separacao">
+                    <label for="">Nome:</label>
+                    <input type="text" name="nome" id="nome">
+                </div>
+                <div class="separacao">
+                    <label for="">Email:</label>
+                    <input type="email" name="email" id="email">   
+                </div>
+                <div class="separacao">
+                    <label for="">Senha:</label>
+                    <input type="password" name="senha" id="senha">   
+                </div>
+                    <button type="submit">Enviar</button>
+            </form>
         </div>
-        <form action="cadastro.php" method="post" class="form">
-            <div class="form-control">
-                <label for="username">Nome de usário</label>
-                <input type="text" name="nome" id="nome">
-            </div>
-            <div class="form-control">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email">
-            </div>
-            <div class="form-control">
-                <label for="Password">senha</label>
-                <input type="password" name="senha" id="senha">
-            </div>
-            <button type="submit">Enviar</button>
-        </form>
     </div>
 </body>
 </html>

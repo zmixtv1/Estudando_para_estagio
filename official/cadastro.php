fcbf314aa216fcf5609d821a291307d63b75f02b
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2022-05-06 12:59:44 -0300

    Update cadastro.php

--- official/cadastro.php
+++ official/cadastro.php
@@ -1,13 +1,26 @@
-<?php
-session_start();
- include("conexao.php");
+<?php 
+include('conexao.php');
 
- $nome = $_POST['nome'];
- $email = $_POST['email'];
+$nome = $_POST['nome'];
+$email = $_POST['email'];
 $senha = $_POST['senha'];
 
-$sql = "INSERT INTO usuarios(nome, email, senha) VALUES ('$nome', '$email', '$senha')";
 
-if ($conexao->query($sql) === TRUE){
-    echo('Cadastrado com sucesso');
+$sqlUsuario = " SELECT * FROM usuarios WHERE email = '$email'";
+
+
+$result = $conexao->query($sqlUsuario);
+
+if(mysqli_num_rows($result) < 1){
+    $sql = "INSERT INTO usuarios(nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+
+    if ($conexao->query($sql) === TRUE){
+        echo('Cadastrado com sucesso');
+    }
+
+}else{
+    echo '<p>Usuário já cadastrado</p>';    
+    exit();
 }
+
+?>

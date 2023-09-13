6ffd92142398e81dfefbdb46b8b207b82c94dd5d
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2023-09-13 00:27:30 -0300

    eestudando python

---
+++ CursoUdemy/PrimeiroEX.py
@@ -0,0 +1,14 @@
+
+name = str("Rodrigo")
+surname = str("Alaor")
+years = int(21)
+year_of_birth = int(2002)
+of_legal_age = bool(2023-2002 >= 18)
+height_in_meters = float(1.85)
+
+print(f"Nome: {name}")
+print(f"Sobrenome: {surname}")
+print(f"Idade: {years}")
+print(f"Ano de Nascimento: {year_of_birth}")
+print(f"E maior de idade: {of_legal_age}")
+print(f"Altura em metros: {height_in_meters}")
---
+++ CursoUdemy/imc.py
@@ -0,0 +1,7 @@
+nome = str("Rodrigo")
+altura = float(1.83)
+peso = int(52)
+imc = peso / (altura**2)
+
+
+print(f"O {nome} tem {altura}\npesa {peso} quilos\ne tem o Imc de {imc:.2f}")

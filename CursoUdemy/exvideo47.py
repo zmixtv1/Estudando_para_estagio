45bffb0399f7d0788fdef69c80d822997f8a7ed5
Rodrigo_Alaor <rodrigoalaor.lm@gmail.com>
2023-10-02 18:34:45 -0300

    Update exvideo47.py

--- CursoUdemy/exvideo47.py
+++ CursoUdemy/exvideo47.py
@@ -1,6 +1,13 @@
 
-name = input(str("Digite o nome: "))
-age = input(int("Digite sua idade: "))
-
+name = str(input("Digite seu nome: "))
+age = int(input("Digite sua idade: "))
+if " " in name:
+    space = "Contem"
+else:
+    space = "Não Contem"
 print(f"Seu nome é {name}")
-print(f"Seu nome invertido é {name:<name}")
+print(f"Seu nome invertido é {name[::-1]}")
+print(f"Seu nome {space} espaços")
+print(f"Seu nome tem {len(name)} letras")
+print(f"A primeira letra do seu nome é {name[0]}")
+print(f"A ultima letra do seu nome é {name[-1]}")
